0303f56a03a05bb79bf7ac5598757e451ae5b85e
CHAERYEON-LEE <sogogijjang@naver.com>
2023-10-22 15:24:29 +0900

    Feat: 타입스크립트 추가 스캐폴디

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,82 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Pagination from "./App";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const PAGE_NUMBER_TEST_ID = "page-number";
+
+describe("Pagination", () => {
+  test("Pagination 컴포넌트 렌더링", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={3}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+
+    // assert
+    const prevButton = screen.getByText(/previous/i);
+    const nextButton = screen.getByText(/next/i);
+
+    const pageNumbers = screen.getAllByTestId(PAGE_NUMBER_TEST_ID);
+
+    pageNumbers.forEach((pageNumber, i) => {
+      expect(pageNumber).toHaveTextContent(`${i + 1}`);
+    });
+
+    expect(prevButton).toHaveClass("disabled");
+    expect(nextButton).not.toHaveClass("disabled");
+  });
+
+  test("첫번째 페이지에서는 이전 페이지로 돌아갈 수 없음", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={3}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+
+    // assert
+    const prevButton = screen.getByText(/previous/i);
+    fireEvent.click(prevButton);
+
+    expect(prevButton).toHaveClass("disabled");
+  });
+
+  test("중간 페이지에서는 이전, 다음 페이지로 이동할 수 있음", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={2}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+
+    // assert
+    const prevButton = screen.getByText(/previous/i);
+    const nextButton = screen.getByText(/next/i);
+
+    fireEvent.click(nextButton);
+
+    expect(prevButton).not.toHaveClass("disabled");
+    expect(nextButton).not.toHaveClass("disabled");
+  });
+
+  test("마지막 페이지에서는 다음 버튼을 클릭했을 때 다음 페이지로 이동할 수 없음", () => {
+    render(
+      <Pagination
+        totalItems={6}
+        itemsPerPage={3}
+        pageNumberTestId={PAGE_NUMBER_TEST_ID}
+      />
+    );
+
+    // assert
+    const nextButton = screen.getByText(/next/i);
+    fireEvent.click(nextButton);
+
+    expect(nextButton).toHaveClass("disabled");
+  });
 });
